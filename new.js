ec9fd42621060cd03d6de8c6801293d397622287
SonikaSahana <deoghariasonika@gmail.com>
2024-01-14 20:46:23 +0530

    newnode

---
+++ new.js
@@ -0,0 +1,12 @@
+
+const student={
+     firstName:"Sonika",
+     lastName:"Sahana", 
+     marks:50, 
+     rollno:10,
+
+greet(){
+console.log('Hi! i am '+this.firstName+ ' ' + this.lastName);
+}
+};
+student.greet();
